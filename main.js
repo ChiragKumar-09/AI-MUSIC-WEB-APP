b594e0ac1d14e5237551ae0a22829685786beb29
ChiragKumar-09 <71137952+ChiragKumar-09@users.noreply.github.com>
2020-09-15 20:47:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,57 @@
-
-Sound = "";
+nothings = "";
+paint_my_love = "";
+right_here = "";
+baby = "";
 leftWristX = 0;
-rightWristX = 0;                  
 leftWristY = 0;
-rightWristY = 0;      
-
-function preload() {
-    Sound = loadSound("music.mp3");
-}
+rightWristX = 0;
+rightWristY = 0;
+leftElbowX = 0;
+leftElbowY = 0;
+rightElbowX = 0;
+rightElbowY = 0;
 
 function setup() {
-    canvas = createCanvas(600 , 500);
+    canvas = createCanvas(450 , 370);
     canvas.center();
 
     video = createCapture(VIDEO);
-    video.hide()
+    video.hide();
 
     poseNet = ml5.poseNet(video , modelLoaded);
-    poseNet.on('pose' , gotPoses);
+    poseNet.on('pose' , gotPoses)
 }
-
 function draw() {
-    image(video , 0 , 0 , 600 , 500);
+   image(img , 0 , 0, 450 , 400);
+   image(video , 25 , 15, 400 , 320);  
 }
-
-function play() {
-    Sound.play();
-    Sound.setVolume(1);
-    Sound.rate(1);
+function preload() {
+   nothings = loadSound("Justin Bieber.mp3") ;
+   paint_my_love = loadSound("NOTHING.mp3") ;
+   right_here = loadSound("paint_my_love.mp3") ;
+   baby = loadSound("right_here.mp3") ;
+    img  = loadImage("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6dkzQYnuyzlbg7vNZdXMDLakdEEnjcuuwBg&usqp=CAU");
 }
 
 function modelLoaded() {
-    console.log("poseNet is initialized");
+   console.log("poseNet is initialized !")
 }
 function gotPoses(results) {
-    if(results.length > 0) {
-       console.log(results);
+        if(results.length > 0) {
+            console.log(results);
 
-       leftWristX = results[0].pose.leftWrist.x;
-       leftWristY = results[0].pose.leftWrist.y;
-       rightWristX = results[0].pose.rightWrist.x;
-       rightWristY = results[0].pose.rightWrist.y;
+            leftWristX = results[0].pose.leftWrist.x;
+            leftWristY = results[0].pose.leftWrist.y;
+            rightWristX = results[0].pose.rightWrist.x;
+            rightWristY = results[0].pose.rightWrist.y;
+            leftElbowX = results[0].pose.leftElbow.x;
+            leftElbowY = results[0].pose.leftElbow.y;
+            rightElbowX = results[0].pose.rightElbow.x;
+            rightElbowY = results[0].pose.rightElbow.y;
 
-       console.log("leftWristX = "+leftWristX+"leftWristY = "+leftWristY);
-       console.log("rightWristX = "+rightWristX+"rightWristY = "+rightWristY);
-    }
+            console.log("leftWristX = "+leftWristX+"leftWristY = "+leftWristY);
+            console.log("rightWristX = "+rightWristX+"rightWristY = "+rightWristY);
+            console.log("leftElbowX = "+leftElbowX+"leftElbowY = "+leftElbowY);
+            console.log("rightElbowX = "+rightElbowX+"rightElbowY = "+rightElbowY);
+        }
 }
